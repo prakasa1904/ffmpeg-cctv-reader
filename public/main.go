0f3043c61119440ff6c281010d983686ac2359f5
Prakasa <prakasa@devetek.com>
2025-11-09 12:07:23 +0700

    fix: split dashboard from video stream bandwith

---
+++ public/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// set app port from env port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	// Create a file server to serve files from the "public" directory
+	fs := http.FileServer(http.Dir("public"))
+
+	// Handle requests to /public/ by stripping the prefix and serving files
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
+	// Optionally, serve an index.html file at the root
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "public/index.html")
+			return
+		}
+		// If not the root, let the static file server handle it (if applicable)
+		fs.ServeHTTP(w, r)
+	})
+
+	log.Printf("Server starting on %s", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ public/scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+go build -o cctv-dashboard public/main.go
+
